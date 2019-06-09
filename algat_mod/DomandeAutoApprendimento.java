ddba95b5ca00c23971b5f6ed49002783f15a0aae
Mango5 <chiara.mengoli5@studio.unibo.it>
2019-06-09 23:12:50 +0200

    Add files via upload

---
+++ algat_mod/DomandeAutoApprendimento.java
@@ -0,0 +1,79 @@
+package algat_mod;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class DomandeAutoApprendimento implements EventHandler<ActionEvent>  {
+    protected Button button;
+	protected Label[] label=new Label[2];
+	protected FileReader file;
+	protected BufferedReader reader;
+	protected TextField testo;
+	protected Stage stage;
+	protected Boolean Spiegazione;
+	public DomandeAutoApprendimento() throws IOException {								//crea una pagina con 2 label, una per la domanda e una per la spiegazione
+		label[0]=new Label();												//una casella di testo per la risposta e un bottone per verificarla
+		this.label[1]=new Label();											//inoltre carica il file dove sono contenute le informzaioni
+		this.testo=new TextField();
+		this.file=new FileReader(""); 
+		this.reader=new BufferedReader(file);
+		this.stage=new Stage();
+		this.Spiegazione=false;
+		stage.setTitle("Domande");
+		label[0].setText(reader.readLine());
+		label[1].setLayoutY(200);
+		button=new Button();
+		button.setText("controlla");
+		button.setLayoutY(100);
+		button.setText("Click me");
+		button.setOnAction(this);
+		Pane layout=new Pane();
+		layout.getChildren().add(button);
+		layout.getChildren().add(label[0]);
+		layout.getChildren().add(label[1]);
+		testo.setLayoutY(50);
+		layout.getChildren().add(testo);
+		Scene scene=new Scene(layout,300,250);
+		this.stage.setScene(scene);
+	};
+	public void vedi() {									//visualizza la pagina creata
+		this.stage.show();
+	}
+	@Override
+	public void handle(ActionEvent event) {                //il file di testo letto in precedenza è scritto in modo che la prima linea sia la domanda, la seconda la risposta e la terza la spiegazione 
+		if(event.getSource()==button) {					   //se la risposta è corretta viene visualizzata la spiegazione e ripremendo sul botone viene visualizzata la domanda successiva
+			try {
+				if(this.Spiegazione==false) {			   
+					reader.mark(10000);
+					if(this.testo.getText().equals(reader.readLine())) {
+						this.label[1].setText(reader.readLine());
+						this.Spiegazione=true;
+						this.button.setText("continua");
+					}else {
+						this.label[1].setText("risposta errata");
+						reader.reset();
+					}
+				}else {
+					this.label[0].setText(reader.readLine());
+					this.label[1].setText("");
+					this.button.setText("controlla");
+					this.Spiegazione=false;
+				}
+			}catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ algat_mod/GraficaAlbero.java
@@ -0,0 +1,138 @@
+package algat_mod;
+
+import algat_mod.tree.*;
+
+import javafx.scene.Group;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+
+public class GraficaAlbero {
+    double rootX;
+    double rootY;
+    double radius; //raggio per tutti i cerchi
+    
+   public GraficaAlbero(){
+        rootX=400;
+        rootY=50;
+        radius=20;
+    }
+            
+    public Group  DisegnaRadice(int key, Group group){
+        Circle root = new Circle();
+        root.setCenterX(rootX);
+        root.setCenterY(rootY);
+        root.setRadius(radius);
+        root.setFill(javafx.scene.paint.Color.BLACK);
+         String chiave = Integer.toString(key);
+        Label label = new Label(chiave);
+         label.setLayoutX(rootX - 9);
+        label.setLayoutY(rootY - 9);
+        label.setTextFill(Color.WHITE);
+         group.getChildren().addAll(root,label);
+         return group;
+    }
+    
+    public Group DisegnaFiglioSx(Nodo node, double parentX, double parentY, Group group){
+        //disegna il ramo
+        Line line=new Line();
+        line.setStartX(parentX);
+        line.setStartY(parentY + radius);
+        line.setEndX(parentX - (double)50);
+        line.setEndY(parentY + radius + (double)50);
+        //disegna il cerchio per il figlio sx
+        Circle childsx = new Circle();
+        childsx.setCenterX(line.getEndX());
+        childsx.setCenterY(line.getEndY()+radius);
+        childsx.setRadius(radius);
+       
+        //inserisce una label contenente la chiave del nodo
+        if (node != null) {
+        	String chiave = Integer.toString(node.key);
+        	Label label = new Label(chiave);
+        	label.setLayoutX(line.getEndX() - 9);
+        	label.setLayoutY(line.getEndY()+radius - 9);
+        	label.setTextFill(Color.WHITE);
+                 childsx.setFill(node.color);
+        	group.getChildren().addAll(line, childsx,label);
+        }
+        else {
+        	Label label = new Label("NIL");
+        	label.setLayoutX(line.getEndX() - 9);
+        	label.setLayoutY(line.getEndY()+radius - 9);
+        	label.setTextFill(Color.WHITE);
+                 childsx.setFill(Color.GREY);
+        	group.getChildren().addAll(line, childsx,label);
+        }
+        return group;
+    }
+    
+    
+    public Group DisegnaFiglioDx(Nodo node, double parentX, double parentY,Group group){
+        Line line=new Line();
+        line.setStartX(parentX);
+        line.setStartY(parentY + radius);
+        line.setEndX(parentX + (double)50);
+        line.setEndY(parentY + radius + (double)50);
+        
+        Circle childdx = new Circle();
+        childdx.setCenterX(line.getEndX());
+        childdx.setCenterY(line.getEndY()+radius);
+        childdx.setRadius(radius);
+       
+        
+       //inserisce una label contenente la chiave del nodo
+        if (node != null) {
+        	String chiave = Integer.toString(node.key);
+        	Label label = new Label(chiave);
+        	label.setLayoutX(line.getEndX() - 9);
+        	label.setLayoutY(line.getEndY()+radius - 9);
+        	label.setTextFill(Color.WHITE);
+                 childdx.setFill(node.color);
+        	group.getChildren().addAll(line, childdx,label);
+        }
+        else {
+        	Label label = new Label("NIL");
+        	label.setLayoutX(line.getEndX() - 9);
+        	label.setLayoutY(line.getEndY()+radius - 9);
+        	label.setTextFill(Color.WHITE);
+                 childdx.setFill(Color.GREY);
+        	group.getChildren().addAll(line, childdx,label);
+        }
+        return group;
+    }
+    
+    public Group DisegnaFigli(Nodo node, double parentX, double parentY, Group group) {
+        if(node == null)
+                return group;
+        else{
+            group = this.DisegnaFiglioSx(node.left, parentX, parentY, group);
+            group = this.DisegnaFiglioDx(node.right, parentX, parentY, group);          
+        }
+        if(node.left != null)
+                group = this.DisegnaFigli(node.left, parentX, parentY, group);
+        if(node.right != null)
+                group = this.DisegnaFigli(node.right, parentX, parentY, group);
+       /* 
+    	if (node.left != null) {
+    		group = this.DisegnaFiglioSx(node.left, parentX, parentY, group);
+    		group = this.DisegnaFigli(node.left, parentX, parentY, group);
+    	}
+    	else
+    		group = this.DisegnaFiglioSx(node.left, parentX, parentY, group);
+    	if (node.right != null) {
+    		group = this.DisegnaFiglioDx(node.right, parentX, parentY, group);
+    		group = this.DisegnaFigli(node.right, parentX, parentY, group);
+    	}
+    	else
+    		group = this.DisegnaFiglioDx(node.right, parentX, parentY, group);*/
+    	return group;
+    }
+    
+    public Group DisegnaAlbero(RedBlackTree tree, Group group){
+        group = this.DisegnaRadice(tree.root.key, group);
+        group = this.DisegnaFigli(tree.root, rootX, rootY, group);
+        return group;
+    }
+}
